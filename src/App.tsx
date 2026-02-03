87431630be0773aa659ce16dcbdb7e35da6aab9f
juhee2341 <210421zz@gmail.com>
2026-02-03 21:14:15 +0900

    Feat: style

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,86 @@ import { Button } from "./components/Button";
  * 그리고 버튼의 대표적인 속성들이 무엇이 있는지 살펴볼것
  */
 function App() {
-  const items = Array.from({ length: 9 }, (_, i) => `상품 ${i + 1}`);
+  const items = Array.from({ length: 9 }, (_, i) => ({
+    name: `Item ${i}`,
+    price: 10000 * i,
+    imageUrl:
+      "https://images.pexels.com/photos/27503501/pexels-photo-27503501.png",
+  }));
+  console.log("items", items);
   const [cart, setCart] = useState<string[]>([]);
 
   const addToCart = (item: string) => {
     setCart((prev) =>
-      prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item]
+      prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item],
     );
   };
 
   return (
     <div className="layout">
       <header>
-        <h1>쇼핑 페이지</h1>
+        <h1>LoVe It</h1>
+        <nav className="nav">
+          <ul className="menu">
+            <li>
+              <a href="#">Overview</a>
+            </li>
+            <li>
+              <a href="#">Online Shop</a>
+            </li>
+            <li>
+              <a href="#">Help</a>
+            </li>
+          </ul>
+        </nav>
       </header>
 
       <main>
         <div className="grid">
           {items.map((item) => (
-            <div key={item} className="card" onClick={() => addToCart(item)}>
-              <span>{item}</span>
-              {cart.includes(item) && <span>☑️</span>}
+            <div
+              key={item.name}
+              className="card"
+              onClick={() => addToCart(item.name)}
+            >
+              <img
+                src={item.imageUrl}
+                height={300}
+                width={200}
+                alt="item_image"
+              />
+              <div className="item">
+                <span>{item.name}</span>
+                <span>{`${item.price.toLocaleString()} 원`}</span>
+              </div>
+              <div className=".icon">
+                <img
+                  src="src/assets/images/heart.png"
+                  alt="cart_image"
+                  height={14}
+                  width={14}
+                />
+                <img
+                  src="src/assets/images/cart.png"
+                  alt="cart_image"
+                  height={14}
+                  width={14}
+                />
+              </div>
             </div>
           ))}
         </div>
 
         <aside className="cart">
-          <strong>장바구니</strong>
+          <div className="icon">
+            <img
+              src="src/assets/images/cart.png"
+              alt="cart_image"
+              height={18}
+              width={18}
+            />
+            <strong>장바구니</strong>
+          </div>
           <ul>
             {cart.map((item, index) => (
               <li key={`${item}-${index}`}>{item}</li>
